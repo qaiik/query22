2f3aefd58c436ad4c8ea6143cba7b31930e67924
qaiik <82181231+qaiik@users.noreply.github.com>
2022-12-09 18:51:54 -0700

    Create unm.js

---
+++ unm.js
@@ -0,0 +1,54 @@
+const $utils = {
+  camel: e=>e.replace(/\b-([a-z])/g,(e,a)=>a.toUpperCase())
+}
+
+class q22 {
+  constructor(iden) {
+    this.element = iden instanceof q22 ? iden.element : iden instanceof HTMLElement || iden instanceof HTMLDocument ? iden : document.querySelector(iden)
+  }
+
+  text(innerText) {
+    this.element.innerText = innerText;
+    return this
+  }
+
+  html(innerHTML) {
+    this.element.innerHTML = innerHTML;
+    return this
+  }
+
+  wrap(tag) {
+    const newElement = document.createElement(tag);
+    this.element.parentNode.insertBefore(newElement, this.element)
+    newElement.appendChild(this.element);
+    return this
+  }
+
+  style(property, value) {
+    let camelizedProperty = $utils.camel(property);
+    this.element.style[camelizedProperty] = value;
+    return this
+  }
+}
+
+function q22a(iden) {
+  let element = Array.isArray(iden) ? iden : Array.from(document.querySelectorAll(iden))
+  return new Proxy(q22.prototype, {
+    get(target, property) {
+      if (typeof Reflect.get(target, property) == "function") {
+        return function(...args) {
+          for (const e of element) {
+            q22.prototype[property].call({element:e}, ...args)
+          }
+          return q22a(element)
+        }
+      } else if (property == "element") {
+          return element
+      }
+    }
+  })
+}
+
+
+let $ = i => new q22(i)
+let $$ = i => q22a(i)
